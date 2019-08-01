ce1c5a03292fef915d52a9de5c254c0dec508ee4
Anuj Datar <anuj.datar@gmail.com>
2019-08-01 08:44:37 -0400

    added code from electron-store for initial testing

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import path from 'path'
+import AppConfig from '@anujdatar/appconfig'
+
+const electron = require('electron')
+
+class ElectronAppConfig extends AppConfig {
+  constructor (options) {
+    options = {
+      ...options
+    }
+    const defaultCwd = (electron.app || electron.remote.app).getPath('userData')
+
+    if (options.configDir) {
+      options.configDir = path.isAbsolute(options.configDir) ? options.configDir : path.join(defaultCwd, options.configDir)
+    } else {
+      options.configDir = defaultCwd
+    }
+    super(options)
+  }
+}
+
+module.exports = ElectronAppConfig
+
+// const test = new ElectronAppConfig()
+
+// console.log(test.configDir)
