57ae2fec73f4ddf81c820e58d1f2eae3eb3a1bad
Anuj Datar <anuj.datar@gmail.com>
2019-08-21 05:01:38 -0400

    code cleanup, remove console logs

--- src/index.js
+++ src/index.js
@@ -9,12 +9,10 @@ class ElectronAppConfig extends AppConfig {
       ...options
     }
     const defaultCwd = (electron.app || electron.remote.app).getPath('userData')
-    console.log('estore defaultCwd', defaultCwd)
 
     if (options.configDir) {
       // if configDir is passed make sure it is an absolute path
       options.configDir = path.isAbsolute(options.configDir) ? options.configDir : path.join(defaultCwd, options.configDir)
-      console.log('estore configDir', options.configDir)
     } else {
       options.configDir = defaultCwd
     }
